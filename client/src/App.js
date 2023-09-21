6b479a3cd7a77a1227ad8bfa92e9d4e34140edd5
AmritHimanshu <amrit.ewp@gmail.com>
2023-09-21 22:53:19 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import AboutPage from './Components/AboutUsPage/AboutPage';
+import CourierService from './Components/CourierServicePage/CourierService';
+import Homepage from './Components/HomePage/Homepage';
+import ServicePage from './Components/ServicePage/ServicePage';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <div className=''>
+
+        <Routes>
+
+          <Route path="/" element={<Homepage />} />
+
+          <Route path="/about" element={<AboutPage />} />
+
+          <Route path="/service" element={<ServicePage />} />
+
+          <Route path="/courier" element={<CourierService />} />
+
+        </Routes>
+
+        <Footer />
+        
+      </div>
+
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/Components/CourierServicePage/Form.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+
+function Form() {
+
+    const [pickup, setPickup] = useState('');
+    const [drop, setDrop] = useState('');
+    const [number, setNumber] = useState('');
+    const [name, setName] = useState('');
+
+    return (
+        <div className='w-[300px] md:w-[450px] py-10 rounded-xl bg-indigo-40 bg-black bg-opacity-60'>
+            <form className='flex flex-col items-center justify-items-center space-y-7 '>
+                <div className='flex flex-col space-y-2'>
+                    <label htmlFor="pickup" className='text-white'>Pickup Location <span className='text-red-500'>*</span></label>
+                    <input
+                        className={`px-2 sm:px-5 py-1 sm:py-2 w-[250px] md:w-[400px] text-black text-md rounded-lg sm:rounded-xl outline-0 placeholder-gray-400 placeholder:italic placeholder:text-sm`}
+                        id="pickup"
+                        type='text'
+                        value={pickup}
+                        name="pickup"
+                        placeholder='Pickup location'
+                        onChange={e => setPickup(e.target.value)}
+                        required
+                    />
+                </div>
+
+                <div className='flex flex-col space-y-2'>
+                    <label htmlFor="drop" className='text-white'>Drop Location <span className='text-red-500'>*</span></label>
+                    <input
+                        className={`px-2 sm:px-5 py-1 sm:py-2 w-[250px] md:w-[400px] text-black text-md rounded-lg sm:rounded-xl outline-0 placeholder-gray-400 placeholder:italic placeholder:text-sm`}
+                        id="drop"
+                        type='text'
+                        value={drop}
+                        name="drop"
+                        placeholder='Drop location'
+                        onChange={e => setDrop(e.target.value)}
+                        required
+                    />
+                </div>
+                
+                <div className='flex flex-col space-y-2'>
+                    <label htmlFor="phone" className='text-white'>Phone Number <span className='text-red-500'>*</span></label>
+                    <input
+                        className={`px-2 sm:px-5 py-1 sm:py-2 w-[250px] md:w-[400px] text-black text-md rounded-lg sm:rounded-xl outline-0 placeholder-gray-400 placeholder:italic placeholder:text-sm`}
+                        id="phone"
+                        type='number'
+                        value={number}
+                        name="phone"
+                        placeholder='Phone number'
+                        onChange={e => setNumber(e.target.value)}
+                        required
+                    />
+                </div>
+                
+                <div className='flex flex-col space-y-2'>
+                    <label htmlFor="name" className='text-white'>Name <span className='text-red-500'>*</span></label>
+                    <input
+                        className={`px-2 sm:px-5 py-1 sm:py-2 w-[250px] md:w-[400px] text-black text-md rounded-lg sm:rounded-xl outline-0 placeholder-gray-400 placeholder:italic placeholder:text-sm`}
+                        id="name"
+                        type='text'
+                        value={name}
+                        name="name"
+                        placeholder='Enter your name'
+                        onChange={e => setName(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type='submit' className='p-2 bg-indigo-500 text-white rounded-lg hover:scale-105 duration-300'>
+                    Submit
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default Form
